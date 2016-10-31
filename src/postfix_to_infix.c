869cb81efa31b5fd58804e902b88005149e77b07
maisam somji <msomji@maisams-MacBook-Pro.local>
2016-10-30 22:35:54 -0400

    minor refactor and reduce excess manual heap memory allocation

--- src/postfix_to_infix.c
+++ src/postfix_to_infix.c
@@ -4,27 +4,27 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-int stackIndex = 0;
-static char* array;
-static int location;
 struct node
 {
   char element;
   struct node *previous;
   struct node *next;
-} *stack[10000000];
+} *stack[110];
 
 typedef struct node node;
-
+int stackIndex = 0;
+char* array;
+int indexLocation = 0;
 node *first_most_node;
-node *last_right_node;
+node *last_most_node;
+
 void push_to_stack(node *link)
 {
   stack[stackIndex] = link;
   stackIndex++;
 }
 
-node *pop()
+node* pop()
 {
   node *lastLink;
   lastLink = stack[stackIndex - 1];
@@ -32,48 +32,46 @@ node *pop()
   return lastLink;
 }
 
-void get_last_node(node *link)
+void set_last_note(node *link)
 {
-  if (link -> next != NULL)
+  if (link->next != NULL)
   {
-    get_last_node(link->next);
+    set_last_note(link->next);
   }
   else
   {
-    last_right_node = link; 
+    last_most_node = link; 
   }
 }
 
-void get_first_node(node *link)
+void set_first_node(node *link)
 {
-  if (link -> previous != NULL)
+  if (link->previous != NULL)
   {
-    get_first_node(link->previous);
+    set_first_node(link->previous);
   }
   else
   {
     first_most_node = link;
   }
 }
-static int status = 0;
-static int locate = 0;
+
 char* recurseList(node* chain)
 {
   if (chain->next)
   {
-    array[locate] = chain->element;
-    locate++;
+    array[indexLocation] = chain->element;
+    indexLocation++;
     recurseList(chain->next);
   }else {
-    array[locate] = chain->element;
+    array[indexLocation] = chain->element;
   }
   return array;
 }
 
 char* getString(node* chain)
 {
-  array = malloc(22300);
-  location = 0;
+  array = malloc(10);
   return recurseList(chain);
 }
 
@@ -91,21 +89,21 @@ char* convert_to_infix(char* postfixArray){
       link -> element = postfixArray[position];
       link -> next = NULL;
       link -> previous  = NULL;
-
       push_to_stack(link);
     } else
     {
       operand1 = pop();
       operand2 = pop();
 
+      set_first_node(operand1);
+      set_last_note(operand2);
+
       link = (node*)malloc(sizeof(node));
       link->element = postfixArray[position];
-      get_first_node(operand1);
       node *lastLink = first_most_node;
       link->next = lastLink;
       lastLink->previous = link;
-      get_last_node(operand2);
-      node *firstLink = last_right_node;
+      node *firstLink = last_most_node;
       link->previous  = firstLink;
       firstLink->next = link;
 
@@ -114,6 +112,6 @@ char* convert_to_infix(char* postfixArray){
     }
     position++;
   }
-  get_first_node(pop());
+  set_first_node(pop());
   return getString(first_most_node);
 };
