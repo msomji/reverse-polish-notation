50a7e254983a1da0e684e0754ceb919aef4475b6
maisam somji <msomji@maisams-MacBook-Pro.local>
2016-11-01 22:50:18 -0400

    updated algorithim to allow accurate validation of invalid posfix expression that would also be invalid infix expressions

--- src/validate_postfix.c
+++ src/validate_postfix.c
@@ -1,10 +1,26 @@
 #include <string.h>
+#include <ctype.h>
+#include <stdio.h>
 
 int validate_postfix(char* postfixArray) 
 {
-  if (postfixArray == "a+b" || strlen(postfixArray) < 3)
+  int counter = 0;
+
+  for (int i =0; i < strlen(postfixArray); i++)
   {
-    return 1;
+
+    if (isalnum(postfixArray[i]))
+    {
+      counter++;
+    }
+    else {
+
+      counter-=2;
+      counter++;
+    }
   }
+
+  if (counter != 1 || strlen(postfixArray) < 3) { return 1; }
+
   return 0;
 }
