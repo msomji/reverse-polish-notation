59a843cb9c199d6cd72b54364ccf6c9b611494e9
maisam somji <msomji@maisams-MacBook-Pro.local>
2016-11-02 00:03:27 -0400

    add sudo code to determine path to validate infix array

--- src/validate_infix.c
+++ src/validate_infix.c
@@ -1,4 +1,17 @@
-
+/*
+ * check valid parenthesis:
+ * initialize counter, 
+ * open parens++;
+ * close parens--;
+ * if parens value ever less than 0, invalid
+ * if parens == 0 therefore valid, 
+ *
+ * this will only work if you limit the shortest array to be 3 (without including parenthesis)
+ *
+ * validate operands:
+ * only two situations: either is is between parenthesis or oparators.
+ * the parenthesis on eitherside cant be the same type.
+ */
 
 int validate_infix(char* infixArray)
 {
