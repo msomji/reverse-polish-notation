5da97bc967b5fc7df832be748d2c9934475f4e01
maisam somji <msomji@maisams-MacBook-Pro.local>
2016-11-03 16:55:22 -0400

    minor clean up of validate infix function

--- src/validate_infix.c
+++ src/validate_infix.c
@@ -1,49 +1,36 @@
-/*
- * check valid parenthesis:
- * initialize counter, 
- * open parens++;
- * close parens--;
- * if parens value ever less than 0, invalid
- * if parens == 0 therefore valid, 
- *
- * this will only work if you limit the shortest array to be 3 (without including parenthesis)
- *
- * validate operands:
- * only two situations: either is is between parenthesis or oparators.
- * the parenthesis on eitherside cant be the same type.
- */
-
 #include <string.h>
 #include <ctype.h>
+
 int validate_infix(char* infixArray)
 {
   int counter= 0;
-  int length = 0;
+  int arrayLength = 0;
+
   for (int i =0; i < strlen(infixArray); i++)
   {
-    if (infixArray[i] == '(')
+    char currentElement = infixArray[i];
+    if (currentElement == '(')
     {
+      if (infixArray[i + 1] == ')') { return 1; };
       counter++;
-        if (infixArray[i + 1] == ')') { return 1; };
-
     }
-    else if (infixArray[i] == ')')
+    else if (currentElement == ')')
     {
-      if (counter <= 0) { return 1;  };
+      if (counter <= 0) { return 1; };
       counter--;
     }
-    else if (!isalnum(infixArray[i]))
+    else if (!isalnum(currentElement))
     {
-        length++;
-        if (infixArray[i - 1] == '(' || infixArray[i + 1] == ')') { return 1; };
+      arrayLength++;
+      if (infixArray[i - 1] == '(' || infixArray[i + 1] == ')') { return 1; };
     }
     else
     {
-      length++;
-        if (infixArray[i - 1] == ')' || infixArray[i + 1] == '(') { return 1; };
+      arrayLength++;
+      if (infixArray[i - 1] == ')' || infixArray[i + 1] == '(') { return 1; };
     }
   }
-  if (length < 3 || counter != 0 ) 
+  if (arrayLength < 3 || counter != 0 ) 
   {
     return 1;
   }
